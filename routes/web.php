198d20217417a56753375dc62c9afdfd2fe2db1a
Kenichi Kanai <kenichi.kanai@gmail.com>
2018-05-14 10:53:11 +0900

    25. Service Providers Explained

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,11 @@
 |
 */
 
-App::bind('App\Billing\Stripe', function() {
-    return new App\Billing\Stripe(config('services.stripe.secret'));
-});
 
 //$stripe = App::make('App\Billing\Stripe');
-$stripe = resolve('App\Billing\Stripe');
+// $stripe = resolve('App\Billing\Stripe');
 //$stripe = app('App\Billing\Stripe');
-
+// dd(resolve('App\Billing\Stripe'));
 //dd($stripe);
 
 
